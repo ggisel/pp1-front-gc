f0f697fd7f88de694b75780934724fbce5f69e66
GabiiF <gabriela.fernandez.92@live.com.ar>
2023-05-04 22:21:34 -0300

    creo carpeta y archivo footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,16 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer className="bg-dark py-3">
+      <div className="container">
+        <p className="text-center text-white">
+          © 2023 MiEmpresa. Todos los derechos reservados.
+        </p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
