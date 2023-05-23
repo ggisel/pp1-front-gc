401a20f167c22bf7b0fd32fd01589a78fbde212a
GabiiF <gabriela.fernandez.92@live.com.ar>
2023-05-23 16:07:00 -0300

    arreglo la vista y datos de la ventana del auto seleccionado

--- src/components/Auto.js
+++ src/components/Auto.js
@@ -28,18 +28,22 @@ const [show, setShow] = useState(false);
           <Col id="datos-auto" xs={12} md={6}>
             {/* Acá agrego los demás datos del auto */}
             <h1>{autoSelected.marca}</h1>
-            <h2>{autoSelected.modelo}</h2>
-            <h2>{autoSelected.anio}</h2>
-            <h2>{autoSelected.precio}</h2>
-            <h2>Importado:{autoSelected.importado}</h2>
-            <h2>Informacion Técnica:</h2>
-            <h2>{autoSelected.combustible}</h2>
-            <h2>Kilometraje: {autoSelected.kilometraje}</h2>
+            <h2 style={{ fontSize: '20px', color: 'gray' }}>{autoSelected.modelo}</h2>
+            <h2 style={{ fontSize: '20px' }}>{autoSelected.anio}</h2>
+            
+            <h2>${autoSelected.precio}</h2>
+            <br/>
+            <h2 style={{ fontSize: '18px'}}>Informacion Técnica:</h2>
+            <h2 style={{ fontSize: '14px',color: 'gray' }}>{autoSelected.combustible}</h2>
+            <h2 style={{ fontSize: '14px',color: 'gray' }}>Kilometraje: {autoSelected.kilometraje} km</h2>
+            <h2 style={{ fontSize:'14px',color: 'gray' }}>Importado:{autoSelected.importado}</h2>
+
             <Button style={{ fontWeight: 'bold' }} variant="primary">Consulta</Button>{' '}
             <Button style={{ fontWeight: 'bold' }} variant="primary">Reserva $$$</Button> {' '}
             <Button variant="primary" onClick={handleShow}>
-       Observaciones
-      </Button>
+            Observaciones
+            </Button>
+
           </Col>
       </Row>
 
